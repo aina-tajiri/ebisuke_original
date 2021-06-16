82dd0fcec3883d97158977c6030c27f0cc508cfc
aina-tajiri <83165412+aina-tajiri@users.noreply.github.com>
2021-06-16 21:24:48 +0900

    delete_loading

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// $(window).on('load',function(){
-//     var loading = function(){
-//       $('.load').fadeOut('fast');
-//       $('.wrap').fadeIn('slow');
-//     };
-//     setTimeout(loading,4000); // setTimeoutを使って表示時間を設定する
-//   });
